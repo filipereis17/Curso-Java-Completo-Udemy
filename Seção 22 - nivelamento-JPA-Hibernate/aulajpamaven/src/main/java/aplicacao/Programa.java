8d23b93d0122a0ef8db9304b6d65c68c8eabc6a1
Filipe Reis <filipe.reis17@live.com>
2023-05-24 14:23:43 -0400

    Nivelamento JPA/Hibernate Parte 3

---
+++ Seção 22 - nivelamento-JPA-Hibernate/aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -0,0 +1,46 @@
+package aplicacao;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Programa {
+		public static void main(String[] args) {
+				
+		/* Primeira execução
+		 * Pessoa p1 = new Pessoa(null, "Carlos da Slva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");*/
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		/* Primeira execução
+		 * em.getTransaction().begin(); // Toda vez que realizar alguma alteração no banco deve inserir
+		em.persist(p1); 				// o getTransaction.begin e .commit()
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();*/
+		
+		// Segunda execução
+		//Pessoa p = em.find(Pessoa.class, 2);
+		//System.out.println(p);
+		
+		/* Erro
+		 * Pessoa p = new Pessoa(2, null, null);		
+		*/
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.getTransaction().begin();
+		em.remove(p); 
+		em.getTransaction().commit();
+		
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
+		
+	}
+
+}
