2b4be4ae22fbd43993baa05e3e52b333d8e9a1c8
Filipe Reis Figueira <filipe@prodam.am.gov.br>
2023-07-28 14:58:08 -0400

    JDBC3 - Comentários

--- Seção 21 - acesso-a-dados-com-jdbc/jdbc3/src/application/Program.java
+++ Seção 21 - acesso-a-dados-com-jdbc/jdbc3/src/application/Program.java
@@ -14,42 +14,42 @@ import db.DB;
 public class Program {
 
 	public static void main(String[] args) {
+		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); //Formata a data
 		Connection conn = null;
-		PreparedStatement st = null;
+		PreparedStatement st = null; // Permite montar consulta SQL
 		try {
 			conn = DB.getConnection();
 			
-			/* Inserir dados na tabela
+			/*
 			st = conn.prepareStatement(
 					"INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
 					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)", //Campos da tabela
-					Statement.RETURN_GENERATED_KEYS
-					);
-			st.setString(1, "Carl Purple"); //1 referencia à primeira '?'
-			st.setString(2, "carl@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+					+ "(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, "Carl Purple");
+			st.setString(2, "carl@gmail");
+			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime())); // Abaixo
 			st.setDouble(4, 3000.0);
 			st.setInt(5, 4);
 			*/
 			
 			st = conn.prepareStatement(
-					"insert into department (Name) values ('D1'), ('D2')",
+					"insert into department (Name) values ('D1'),('D2')",
 					Statement.RETURN_GENERATED_KEYS);
 			
-			int rowsAffected = st.executeUpdate();
-			//System.out.println("Done! Rows affected: " + rowsAffected);
-			
-			if (rowsAffected > 0) {
-				ResultSet rs = st.getGeneratedKeys();
+			int rowsAffected = st.executeUpdate(); //retorna um valor inteiro
+						
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys(); // retorna um objeto do tipo ResultSet
 				while (rs.next()) {
 					int id = rs.getInt(1);
 					System.out.println("Done! Id = " + id);
 				}
 			} else {
-				System.out.println("Nenhuma linha afetada!");
+				System.out.println("No rown affected");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -57,12 +57,22 @@ public class Program {
 		/*catch (ParseException e) {
 			e.printStackTrace();
 		} */
-		
 		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
-
+		
 	}
 
 }
+
+/*
+ * 		sdf.parse("22/04/1985"): Essa parte do código está analisando a string "22/04/1985" 
+ * para criar um objeto java.util.Date. Esse objeto representa a data 22 de abril de 1985, 
+ * mas não possui informações sobre horas, minutos e segundos.
+ * 		new java.sql.Date(...): Essa parte está criando um objeto 
+ * 		java.sql.Date a partir do objeto java.util.Date criado anteriormente. O construtor 
+ * java.sql.Date(long date) espera um valor em milissegundos para inicializar a data SQL. 
+ * A chamada ao método getTime() do objeto java.util.Date retorna o valor em milissegundos 
+ * correspondente a essa data.
+*/
--- Seção 21 - acesso-a-dados-com-jdbc/jdbc3/src/db/DB.java
+++ Seção 21 - acesso-a-dados-com-jdbc/jdbc3/src/db/DB.java
@@ -10,23 +10,27 @@ import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
-	
+
+	// Criar conexão com o BD
 	private static Connection conn = null;
 	
+	// Método para criar coneção
 	public static Connection getConnection() {
 		if (conn == null) {
-			try {				
+			try {
 				Properties props = loadProperties();
-				String url = props.getProperty("dburl");				
-				conn = DriverManager.getConnection(url, props);
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props); // url do BD e propriedades de conexão
 			}
 			catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
+			
 		}
 		return conn;
 	}
 	
+	//Método para fechar conexão
 	public static void closeConnection() {
 		if (conn != null) {
 			try {
@@ -38,18 +42,26 @@ public class DB {
 		}
 	}
 	
+	
+	// Método auxiliar para carregar dados do db.properties
 	private static Properties loadProperties() {
+		
+		// FileInputStream armazena dados do arquivo db.properties
 		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			
+			// Instanciar um objeto do tipo Properties
 			Properties props = new Properties();
-			props.load(fs);
+			props.load(fs); // Carrega arquivo em objeto props 
 			return props;
 		}
+		// Em caso de erro
 		catch (IOException e) {
+			// Exceção personalizada
 			throw new DbException(e.getMessage());
 		}
 	}
 	
-	public static void closeStatement(Statement st ) {
+	public static void closeStatement(Statement st) {
 		if (st != null) {
 			try {
 				st.close();
@@ -60,7 +72,7 @@ public class DB {
 	}
 	
 	public static void closeResultSet(ResultSet rs) {
-		if(rs != null) {
+		if (rs != null) {
 			try {
 				rs.close();
 			} catch (SQLException e) {
@@ -68,4 +80,5 @@ public class DB {
 			}
 		}
 	}
+
 }
