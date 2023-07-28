9f7feb778f04402fbe4de6504aae469959b68169
Filipe Reis Figueira <filipe@prodam.am.gov.br>
2023-07-28 14:07:15 -0400

    JDBC2 - Adição de comentários sobre os comandos

--- Seção 21 - acesso-a-dados-com-jdbc/jdbc2/src/application/Program.java
+++ Seção 21 - acesso-a-dados-com-jdbc/jdbc2/src/application/Program.java
@@ -13,27 +13,31 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Connection conn = null; //Conectar ao banco
-		Statement st = null; //Consulta ao SQL
-		ResultSet rs = null; //Armazena o resultado da consulta
-		
+		Statement st = null; 	//Consulta ao SQL
+		ResultSet rs = null;	//Armazena o resultado da consulta
 		try {
 			conn = DB.getConnection();
 			
+			// Instanciar um objeto do tipo Statement
 			st = conn.createStatement();
-			//rs = st.executeQuery("select *from department");
-			rs = st.executeQuery("select Id, Name, Email from seller");
 			
-			while (rs.next()) {
-				//System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
-				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name") + ", " + rs.getString("Email"));
+			// st já instanciada
+			// rs recebe o resultado
+			rs = st.executeQuery("select * from department"); // método espera uma string
+			
+			//rs começa na posição 0
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
 		finally {
+			// Fechar conexão por método para evitar ter que criar try catch para cada um
 			DB.closeResultSet(rs);
 			DB.closeStatement(st);
-			DB.closeConnection();			
+			DB.closeConnection();
 		}
 
 	}
