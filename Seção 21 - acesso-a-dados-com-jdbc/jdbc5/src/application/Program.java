c859a917d3839d8b81f96e3b6afff11193a4cc96
Filipe Reis <filipe.reis17@live.com>
2023-08-04 14:31:19 -0400

    JDBC5 - Comentários

--- Seção 21 - acesso-a-dados-com-jdbc/jdbc5/src/application/Program.java
+++ Seção 21 - acesso-a-dados-com-jdbc/jdbc5/src/application/Program.java
@@ -11,24 +11,29 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		// Inicializa as variáveis para conexão com o banco de dados e declaração preparada
 		Connection conn = null;
 		PreparedStatement st = null;
 		try {
+			// Estabelece a conexão com o banco de dados
 			conn = DB.getConnection();
 			
+			// Cria uma declaração preparada para excluir um departamento
 			st = conn.prepareStatement(
 					"DELETE FROM department "
 					+ "WHERE "
-					+ "Id = ?"
-			);
-			st.setInt(1, 5); //Apagou department D1
-			//st.setInt(1, 2);
-							
+					+ "Id = ?");
 			
+			//st.setInt(1, 5);
+			st.setInt(1, 2);
+			
+			// Executa a deleção e obtém o número de linhas afetadas
 			int rowsAffected = st.executeUpdate();
 			
+			// Imprime o resultado, mostrando quantas linhas foram afetadas
 			System.out.println("Done! Rows affected: " + rowsAffected);
 		} catch (SQLException e) {
+			// Lança erro personalizado caso exista problema de integridade
 			throw new DbIntegrityException(e.getMessage());
 		} 
 		finally {
