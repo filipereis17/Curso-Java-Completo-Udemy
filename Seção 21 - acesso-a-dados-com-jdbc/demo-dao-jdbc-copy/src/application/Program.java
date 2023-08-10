9c26841e79a609be5f4b7e33f63781e20a16a0ca
Filipe Reis <filipe.reis17@live.com>
2023-08-10 11:50:31 -0400

    Implementação findByDepartment

--- Seção 21 - acesso-a-dados-com-jdbc/demo-dao-jdbc-copy/src/application/Program.java
+++ Seção 21 - acesso-a-dados-com-jdbc/demo-dao-jdbc-copy/src/application/Program.java
@@ -1,20 +1,32 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
-		System.out.println("=== TEST 1: seller findById ===");
-		
-		Seller seller = sellerDao.findById(3);
-		
-		System.out.println(seller);
-	}
+		// Criando uma instância do SellerDao usando a fábrica DaoFactory
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println("=== TEST 1: seller findById ===");
+        // Testando o método findById do SellerDao para buscar um vendedor por ID
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
+
+        System.out.println("\n=== TEST 2: seller findByDepartment ===");
+        // Criando um departamento com ID 2 para usar como filtro na busca
+        Department department = new Department(2, null);
+        // Testando o método findByDepartment do SellerDao para buscar vendedores por departamento
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+    }
 
 }
