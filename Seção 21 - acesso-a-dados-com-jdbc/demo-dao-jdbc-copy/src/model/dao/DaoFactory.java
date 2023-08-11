04a0c9ccd917bdd1b2c22f21587135b7aef7e042
Filipe Reis <filipe.reis17@live.com>
2023-08-11 10:27:59 -0400

    Create DepartmentDaoJDBC, DaoFactory and Program2

--- Seção 21 - acesso-a-dados-com-jdbc/demo-dao-jdbc-copy/src/model/dao/DaoFactory.java
+++ Seção 21 - acesso-a-dados-com-jdbc/demo-dao-jdbc-copy/src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 //Declaração da classe DaoFactory, responsável por criar instâncias dos DAOs
@@ -10,5 +11,9 @@ public class DaoFactory {
  public static SellerDao createSellerDao() {
      // Cria uma nova instância da classe de implementação SellerDaoJDBC
      return new SellerDaoJDBC(DB.getConnection());
+ }
+ 
+ public static DepartmentDao createDepartmentDao() {
+	 return new DepartmentDaoJDBC(DB.getConnection());
  }
 }
