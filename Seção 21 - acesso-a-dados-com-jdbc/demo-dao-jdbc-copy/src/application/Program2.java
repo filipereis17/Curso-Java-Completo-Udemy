4d7dc10cc37ca43d17bfb915e092a6a3bd09d7f7
Filipe Reis <filipe.reis17@live.com>
2023-08-11 11:36:21 -0400

    Implementation departmentDao.findAll

--- Seção 21 - acesso-a-dados-com-jdbc/demo-dao-jdbc-copy/src/application/Program2.java
+++ Seção 21 - acesso-a-dados-com-jdbc/demo-dao-jdbc-copy/src/application/Program2.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -11,9 +13,13 @@ public class Program2 {
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
         System.out.println("=== TEST 1: department findById ===");
-        Department department = departmentDao.findById(1);
+        Department department = departmentDao.findById(2);
         System.out.println(department);
 
+        System.out.println("\n=== TEST 2: department findAll ===");
+        List<Department> list = departmentDao.findAll();
+        for (Department obj : list) {
+        	System.out.println(obj);
+        }
 	}
-
 }
